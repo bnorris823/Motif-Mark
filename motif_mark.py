204ea7671fb93abbe28a67a24b25abd40fe20f7e
Brandon Norris <bnorris823@gmail.com>
2018-02-13 12:53:55 -0800

    added parse_motif function

--- motif_mark.py
+++ motif_mark.py
@@ -1,9 +1,13 @@
 
+
+
+
 def get_parts(line):
     pre_intron = ""
     post_intron = ""
     exon = ""
     ex_found = False
+
     for c in line:
         if c.isupper():
             exon = exon + c
@@ -24,13 +28,29 @@ def fasta_to_dict(file):
         fasta_dict = {}
         header = ""
         seq = ""
-        count = 0
+        first = 0
+
         for line in fh:
             line = line.strip()
             if line[0] == ">":
-                if count > 0:
+                if not first:
                     fasta_dict[header] = seq
             else:
-                seq = line
-                count += 1
+                seq += line
+                first = False
+
     return fasta_dict
+
+def parse_motif(motif):
+    motif_list = []
+    for c in motif:
+        motif_list.append(base_dict[c])
+
+    return motif_list
+
+
+base_dict = {"A": "Aa", "T":"Tt", "C":"Cc", "G":"Gg", "U":"Uu",
+              "R": "AaGg", "Y":"TtCcUu", "S":"CcGg", "W":"AaTtUu",
+              "K":"GgTtUu", "M":"AaCc", "B":"CcGgTtUu", "D":"AaGgTtUu",
+              "H":"AaCcTtUu", "V":"AaCcGg", "N":"AaTtCcGgUu"}
+
