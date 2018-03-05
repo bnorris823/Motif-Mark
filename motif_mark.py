3e0118168a53dd81ec4d3e5e2eab651639d4ad2d
Brandon Norris <bnorris823@gmail.com>
2018-03-05 12:10:10 -0800

    completed part1 of assignment, getting positions of motifs

--- motif_mark.py
+++ motif_mark.py
@@ -1,6 +1,13 @@
 import argparse
-import
+import cairo
 
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Python script to display binding site motifs around exons")
+    parser.add_argument("-f", help="set fasta filename", required=True, type=str)
+    parser.add_argument("-m", help="set motif file", required=False, type=bool)
+
+    return parser.parse_args()
 
 
 def get_parts(line):
@@ -23,6 +30,17 @@ def get_parts(line):
 
 
 
+def get_exon(line):
+    '''finds exon in fasta sequence and return it'''
+    exon = ""
+
+    for c in line:
+        if c.isupper():
+            exon = exon + c
+
+    return exon
+
+
 
 
 def fasta_to_dict(file):
@@ -31,37 +49,109 @@ def fasta_to_dict(file):
         fasta_dict = {}
         header = ""
         seq = ""
-        first = 0
+        first = True
 
         for line in fh:
             line = line.strip()
+
             if line[0] == ">":
                 if not first:
-                    fasta_dict[header] = seq
+                    fasta_dict[header] = get_parts(seq)
+                header = line
+                seq = ""
             else:
                 seq += line
                 first = False
 
+    fasta_dict[header] = seq
     return fasta_dict
 
 
 
 
-def parse_motifs(motif):
-    '''Takes a motif and adds it to a dict with a list of the possible chars at each position as its value.'''
+def parse_motifs(motif_file):
+    '''Takes a list of motifs and creates a list of possible chars for each motif.'''
     motif_list = []
-    for c in motif:
-        motif_list.append(base_dict[c])
+    motif_dict = {}
+    with open(motif_file)as fh:
+        for line in fh:
+            line = line.strip()
+            line = line.upper()
+            motif_list.append(line)
 
-    if motif not in motif_dict:
-        motif_dict[motif] = motif_list
+    for motif in motif_list:
+        chars = []
+        for c in motif:
+            chars.append(base_dict[c])
+        motif_dict[motif] = chars
 
 
+    return motif_dict
+
+
+
+
+
+
+def get_positions(header, seq):
+    positions = {}
+    for motif in motif_dict.keys():
+        chars = motif_dict[motif]
+        window = len(motif)
+        for i in range(0,len(seq) - window):
+            pattern = seq[i:i + window]
+
+            count = 0
+            match = True
+            for c in pattern:
+
+                if c not in chars[count]:
+                    match = False
+                count += 1
+            if match:
+
+                if header + "_" + motif in positions:
+                    positions[header + "_" + motif].append(i)
+                else:
+                    positions[header + "_" + motif] = [i]
+
+    return positions
+
+
+
+def rev_dict(dict):
+    new_dict = {}
+    for key in dict.keys():
+        new_key = dict[key]
+        new_value = key
+        new_dict[new_key] = new_value
+
+    return new_dict
 
 base_dict = {"A": "Aa", "T":"Tt", "C":"Cc", "G":"Gg", "U":"Uu",
               "R": "AaGg", "Y":"TtCcUu", "S":"CcGg", "W":"AaTtUu",
               "K":"GgTtUu", "M":"AaCc", "B":"CcGgTtUu", "D":"AaGgTtUu",
               "H":"AaCcTtUu", "V":"AaCcGg", "N":"AaTtCcGgUu"}
 
-motif_dict = {}
+rev_base_dict = rev_dict(base_dict)
+
+#args = get_arguments()
+
+fasta_dict = fasta_to_dict("test.fa") #args.f
+motif_dict = parse_motifs("motifs_test.txt") #args.m
+fasta_pos_list = []
+
+
+
+for item in fasta_dict.items():
+    seq = "".join(fasta_dict[item[0]])
+    pos = get_positions(item[0], seq)
+    fasta_pos_list.append(pos)
+
+for f in fasta_pos_list:
+    for key in f.keys():
+        print(f[key])
+
+
+
 
